4a7ae7a24fe64d4761066988f74d7381bcfbf075
srinu1657 <srinu1657@gmail.com>
2025-04-17 01:56:22 -0500

    typescript with custome types aliase

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CourseGoal from "./components/CourseGoal";
+const App = () => {
+  return (<main>
+    <CourseGoal title="Learn React + TS" description="Lear it form the group up" id={100}/>
+  </main>);
+}
+export default App;
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,12 @@
+const CourseGoal = ({ title, description }: { title: string; description: string }) => {
+    return (
+        <article>
+            <div>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+            <button>Delete</button>
+        </article>
+    )
+}
+export default CourseGoal;
